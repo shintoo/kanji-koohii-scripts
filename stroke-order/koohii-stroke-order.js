1fc406f5e6454fafb92ff0dd8d32f312f166e6c7
Sean Rapp <seanprapp@gmail.com>
2024-02-02 20:57:01 -0500

    Update koohii-stroke-order.js

--- stroke-order/koohii-stroke-order.js
+++ stroke-order/koohii-stroke-order.js
@@ -1,41 +1,56 @@
-const sleep = ms => new Promise(r => setTimeout(r, ms));
+// ==UserScript==
+// @name         Kanji Koohii Stroke Order
+// @namespace    https://github.com/shintoo/kanji-koohii-scripts
+// @version      1.0
+// @description  Add stroke order in handwritten font to Kanji Koohii cards during review
+// @author       github.com/shintoo
+// @match        https://kanji.koohii.com/review*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
 
-function initialize() {
-  console.log("initializing")
+(function() {
+    'use strict';
 
-  const flipButton = document.getElementById("uiFcButtons0").getElementsByTagName("a")[0]
-  flipButton.addEventListener("click", addHandwriting, false)
-}
+    const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-async function addHandwriting() {
-  await sleep(50)
-  console.log("adding stroke order")
-  const kanjiSpan = getKanjiContainer()
-  const kanji = kanjiSpan.innerText
-  console.log("kanji: " + kanji)
-  const hwKanji = document.createElement("span")
-  hwKanji.style.fontFamily = "KanjiStrokeOrders"
-  hwKanji.innerText = kanji
+    function initialize() {
+        console.log("initializing")
 
-  kanjiSpan.style.fontSize = "0.75em"
-  kanjiSpan.appendChild(hwKanji)
-}
+        const flipButton = document.getElementById("uiFcButtons0").getElementsByTagName("a")[0]
+        flipButton.addEventListener("click", addHandwriting, false)
+    }
 
-function getKanjiContainer() {
-  const container = document.getElementsByClassName("uiFcHalf d-kanji")[0]
-  const span = container.getElementsByClassName("cj-k")[0] 
-  return span 
-}
+    async function addHandwriting() {
+        await sleep(50)
+        console.log("adding stroke order")
+        const kanjiSpan = getKanjiContainer()
+        const kanji = kanjiSpan.innerText
+        console.log("kanji: " + kanji)
+        const hwKanji = document.createElement("span")
+        hwKanji.style.fontFamily = "KanjiStrokeOrders"
+        hwKanji.innerText = kanji
 
-const main = document.getElementById("uiFcMain")
+        kanjiSpan.style.fontSize = "0.75em"
+        kanjiSpan.appendChild(hwKanji)
+    }
 
-const callback = function (mutationsList, observer) {
-  observer.disconnect()
-  for (const mutation of mutationsList) {
-    initialize()
-  }
-};
+    function getKanjiContainer() {
+        const container = document.getElementsByClassName("uiFcHalf d-kanji")[0]
+        const span = container.getElementsByClassName("cj-k")[0]
+        return span
+    }
 
-const config = {attributes: true, childList: true, subtree: true};
-const observer = new MutationObserver(callback);
-observer.observe(main, config);
+    const main = document.getElementById("uiFcMain")
+
+    const callback = function (mutationsList, observer) {
+        observer.disconnect()
+        for (const mutation of mutationsList) {
+            initialize()
+        }
+    };
+
+    const config = {attributes: true, childList: true, subtree: true};
+    const observer = new MutationObserver(callback);
+    observer.observe(main, config);
+})();
